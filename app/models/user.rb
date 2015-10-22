46215be2a6ce06ceb93e21bd94ba01dbbe154982
Douglas Moore <douglas.moore89@gmail.com>
2015-10-22 10:39:38 -0400

    Add session token methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
-  vallidates :email, :password_digest, :session_token, presence: true
-  vallidates :email, uniqueness: true 
+  after_initialize :generate_session_token
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+
+  def generate_session_token
+    self.session_token = SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    session[:token] = self.session_token
+  end
+
+  def ensure_session_token
+    unless session_token == self.session_token
+      flash[:errors] = ["User not logged in"]
+      redirect_to users_url
+    end
+  end
+end
