457eed1d0bab3f388a4b19049dcb2f6928525f6a
Douglas Moore <douglas.moore89@gmail.com>
2015-10-22 11:02:01 -0400

    Finish building user model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ActiveRecord::Base
-  after_initialize :generate_session_token
+  attr_reader :password
+
+  after_initialize :ensure_session_token
   validates :email, :password_digest, :session_token, presence: true
   validates :email, uniqueness: true
+  validates :password, length: { minimum: 6, nil: true }
 
-  def generate_session_token
-    self.session_token = SecureRandom::urlsafe_base64
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return nil unless user
+    return user if user.is_password?(password)
   end
 
   def reset_session_token!
@@ -13,9 +23,17 @@ class User < ActiveRecord::Base
   end
 
   def ensure_session_token
-    unless session_token == self.session_token
-      flash[:errors] = ["User not logged in"]
-      redirect_to users_url
+    if self.session_token.nil?
+      self.session_token = User.generate_session_token
     end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 end
